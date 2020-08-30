a4c1df45fc525f76c0dd0b6e60494c2b59f18a84
Thiago Trabach <thiago.trabach@gmail.com>
2020-08-29 21:28:31 -0300

    improve log message

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-send_to_telegram = False
+send_to_telegram = True
 
 try:
+    if send_to_telegram:
+        telegram.send('{} START: Backtest'.format(telegram.emoji.play))
+
     # load Parameters
     SHEET_PATH = os.getenv('SHEET_PATH')
     PORTFOLIO_MAX_SIZE = eval(os.getenv('PORTFOLIO_MAX_SIZE'))
@@ -24,10 +27,6 @@ try:
     VARIANCIA_BETA = eval(os.getenv('VARIANCIA_BETA'))
     DAGOSTINO_PERSON = eval(os.getenv('DAGOSTINO_PERSON'))
 
-    if send_to_telegram:
-        telegram.send('{} FASE: Inicio do Backtest'.format(telegram.emoji.play))
-
-
     #load data
     df = dl.sheet_importer(SHEET_PATH)
 
@@ -36,13 +35,13 @@ try:
 
     # Get grid search run info
     if send_to_telegram:
-        telegram.send('{} FASE: Estimativa de execução'.format(telegram.emoji.hourglass))
+        telegram.send('{} RUN: Estimate Time to Complete'.format(telegram.emoji.hourglass))
 
     cv.grid_search_info(df, grid, parallel=True, send_to_telegram=send_to_telegram)
 
     # Run grid search
     if send_to_telegram:
-        telegram.send('{} FASE: Execução da Busca'.format(telegram.emoji.hourglass))
+        telegram.send('{} RUN: Grid Search'.format(telegram.emoji.hourglass))
 
     results = cv.grid_search(df, grid, parallel=True, verbose=True, send_to_telegram=send_to_telegram)
 
@@ -52,8 +51,8 @@ try:
     df_results.to_pickle("./outputs/results.pkl")
 
     if send_to_telegram:
-        telegram.send('{} FASE: Fim do Backtest!'.format(telegram.emoji.check))
+        telegram.send('{} END: Succcess'.format(telegram.emoji.check))
 
 except:
-    telegram.send("{} ERROR: {}".format(telegram.emoji.double_exclamation, sys.exc_info()))
+    telegram.send('{} END: Failure'.format(telegram.emoji.cross))
     telegram.send("{} ERROR: {}".format(telegram.emoji.double_exclamation,sys.exc_info()))
