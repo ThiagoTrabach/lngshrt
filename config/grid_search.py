0b512c930d08ec7e53f8f0b68c221ca5b1538507
Thiago Trabach <thiago.trabach@gmail.com>
2020-10-04 19:40:51 -0300

    update gridsearch config

--- config/grid_search.py
+++ config/grid_search.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 class parameters:
-    PORTFOLIO_MAX_SIZE = np.arange(2,4,1)
-    DICKEY_FULLER = np.arange(3,4,1)
-    FISHER = np.arange(-0.5,-0.4,0.1)
-    MEIA_VIDA = [10,25]
-    MEDIA_N = np.arange(0.5, 0.6, 0.1)
-    DESVIO_PADRAO = np.arange(2, 2.1, 0.1)
-    PERIODO = [100]
-    VARIANCIA_BETA = np.arange(0.01, 0.12, 0.1)
+    PORTFOLIO_MAX_SIZE = np.arange(5,8,1)
+    DICKEY_FULLER = np.arange(3,10,1)
+    FISHER = np.arange(0,0.9,0.1)
+    MEIA_VIDA = [10,25,70,100]
+    MEDIA_N = np.arange(0.5, 3.5, 0.5)
+    DESVIO_PADRAO = np.arange(2, 2.6, 0.1)
+    PERIODO = [100,200]
+    VARIANCIA_BETA = np.arange(0.01,1, 0.5)
     DAGOSTINO_PERSON = 0.05
+    PROP_FINANCEIRO = np.arange(2, 6, 1)
